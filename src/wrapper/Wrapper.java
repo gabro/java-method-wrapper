b3cadc0870847710ef0cdc960fb2d61788f1ed4c
Gabriele Petronella <gabriele.petronella@gmail.com>
2012-10-18 18:13:27 -0500

    Fancy explanatory comment

--- src/wrapper/Wrapper.java
+++ src/wrapper/Wrapper.java
@@ -2,6 +2,19 @@ package wrapper;
 
 import java.util.concurrent.Callable;
 
+/*  
+ *  Short comment about this shit
+ * 
+ *  If only Java had lambdas this wouldn't be necessary
+ *  but since this is not the case, we have to pass around
+ *  this awful Callable objects.
+ *  
+ *  Java 1.8 introduced lambdas, but dropping
+ *  the compatibility with Java < 1.8 didn't seem to be
+ *  a brilliant idea...
+ */
+
+
 public class Wrapper {	
 	/**
 	 * This will call the method wrapping it with a preamble and a prologue
